f3b5b87b7288d6e8ff3963101e7953d9a96245c8
Angel Penchev <angel.penchev.tsb@gmail.com>
2023-11-06 19:55:29 +0200

    refactor: renamed 'blog' to 'news'
    Renamed the blog section to news.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,38 @@
-import { defineConfig } from 'astro/config';
+import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+import NetlifyCMS from "astro-netlify-cms";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://example.com',
+    integrations: [
+        mdx(),
+        sitemap(),
+        NetlifyCMS({
+            config: {
+                backend: {
+                    name: 'git-gateway',
+                    branch: 'main',
+                },
+                collections: [
+                    {
+                        name: 'news',
+                        label: 'News Posts',
+                        folder: 'src/content/news',
+                        create: true,
+                        delete: true,
+                        fields: [
+                            {label: 'Title', name: 'title', widget: 'string'},
+                            {label: 'Description', name: 'description', widget: 'string'},
+                            {label: 'Publish Date', name: 'pubDate', widget: 'datetime'},
+                            {label: 'Hero Image', name: 'heroImage', widget: 'image'},
+                            {label: 'Body', name: 'body', widget: 'markdown'},
+                        ],
+                    },
+                ],
+            },
+        }),
+    ],
 });
