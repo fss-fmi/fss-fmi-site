43cbb64ffcfd347b4f75bf324f680b69d7d16dd7
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-01-12 22:20:32 +0200

    feat: admin panel rewrite
    Added a rewrite to map the `/admin` path to the html file of the admin panel.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 const withNextIntl = require('next-intl/plugin')('./app/i18n.ts');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/admin',
+        destination: '/admin/index.html',
+      },
+    ];
+  },
+};
 
 module.exports = withNextIntl(nextConfig);
